8f9b1e802cee25d195e98d1341fff1731e6dba0f
sagersan <sagersan@mail.uc.edu>
2026-09-18 13:09:25 -0400

    Initial commit

---
+++ src/CCBalance.java
@@ -0,0 +1,16 @@
+public class CCBalance
+{
+    static void main()
+    {
+        double balance = 5000; // Initial
+        final double RATE = .17;
+
+        // balance = balance + interest // interest = balance * RATE
+        balance = balance + balance * RATE; // Month one!
+
+        IO.println("The balance after one month is " + balance);
+        balance = balance + balance * RATE;
+        IO.println("The balance after month two is " + balance);
+
+    }
+}
